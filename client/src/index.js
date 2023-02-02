ba15bcf0df08f7882c577a2ac3d45f516f941da4
camerona56 <cameronadamson56@gmail.com>
2023-02-02 10:42:05 +0000

    Handling Payments Completed

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
-import 'materialize-css/dist/css/materialize.min.css';
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import reduxThunk from 'redux-thunk';
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
